659b30023cf1a10be4dd1b7023a028d9aeeb3e2f
Ejup Hoxha <ejup.hoxha@gmail.com>
2021-04-27 01:49:37 -0400

    Tested ResNet50 encoder... Not good! Added Normalization to networks etc. 	new file:   Notebooks/dp_002.ipynb 	new file:   Notebooks/unet256.ipynb 	modified:   dataloaders/dataloaders.py 	new file:   models/res50.py 	new file:   models/test128.png 	new file:   models/test256.png 	new file:   models/testres50.png 	modified:   models/train.py 	renamed:    models/models.py -> models/unet128.py 	new file:   models/unet256.py

--- models/train.py
+++ models/train.py
@@ -1,13 +1,48 @@
 import sys
 sys.path.insert(1, '/tmp/Projects2021/depth_estimation/final-project-monodepth-ccny/dataloaders/')
 from dataloaders import *
-from models import *
+from unet128 import unet128
+from unet256 import unet256
+from res50 import res50
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
 
-dataset_path = '/tmp/Projects2021/rgbd_dataset/indoor_dataset/train_dataset'
-dtloader = dataloarder_rgbd(dataset_path,10)
+dataset_path = '/tmp/Projects2021/rgbd_dataset/dataset2/'
+argv = sys.argv
 
-m = models(128)
-m.unet_resnet50.compile(optimizer='adam', loss='mse')
-m.unet_resnet50.summary()
+if argv[1] == 'unet128':
+    dtloader = dataloader_rgbd(dataset_path, 8, image_size=128)
+    checkpoint = ModelCheckpoint('best_model128.hdf5',
+                                monitor='loss',
+                                save_best_only=True)
 
-m.unet_resnet50.fit_generator(dtloader, samples_per_epoch=50, nb_epoch=3)
+    m = unet128(input_shape=[128, 128, 3])
+    m.model.compile(optimizer='adam', loss='mse')
+    m.model.summary()
+    m.model.fit(dtloader, epochs=int(argv[2]), callbacks=[checkpoint])
+
+elif argv[1] == 'unet256':
+    dtloader = dataloader_rgbd(dataset_path, 8, image_size=256)
+    checkpoint = ModelCheckpoint('best_model256.hdf5',
+                                monitor='loss',
+                                save_best_only=True)
+
+    m = unet256(input_shape=[256, 256, 3])
+    m.model.compile(optimizer='adam', loss='mse')
+    m.model.summary()
+    m.model.fit(dtloader, epochs=int(argv[2]), callbacks=[checkpoint])
+
+elif argv[1] == 'res50':
+    dtloader = dataloader_rgbd(dataset_path, 8, image_size=128)
+    checkpoint = ModelCheckpoint('best_modelres50.hdf5',
+                                monitor='loss',
+                                save_best_only=True)
+
+    m = res50(input_shape=(128, 128, 3))
+    m.model.compile(optimizer='adam', loss='mse')
+    m.model.summary()
+    m.model.fit(dtloader, epochs=int(argv[2]), callbacks=[checkpoint])
+else:
+    print("Command received: ", argv[1])
+    print("\nPlease define the model you want to train and number of epochs!\n")

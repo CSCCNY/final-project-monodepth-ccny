4550274cccde7819f99d0cfa87ac934bbd38b1fa
Ejup Hoxha <ejup.hoxha@gmail.com>
2021-04-29 13:42:31 -0400

    ehoxha (#19)
    * "Accidentally pushed directly to main branch"
    
    * Add files via upload
    
    * Add files via upload
    
    * Finally, huh! Deleted __pycache__ folders!
    	deleted:    dataloaders/__pycache__/dataloaders.cpython-36.pyc
    	renamed:    dataloaders/__pycache__/dataloaders.py -> dataloaders/dataloaders.py
    	deleted:    models/__pycache__/models.cpython-36.pyc
    	deleted:    models/__pycache__/train.py
    	renamed:    models/__pycache__/models.py -> models/models.py
    	new file:   models/train.py
    
    * Tested ResNet50 encoder... Not good!
    Added Normalization to networks etc.
    	new file:   Notebooks/dp_002.ipynb
    	new file:   Notebooks/unet256.ipynb
    	modified:   dataloaders/dataloaders.py
    	new file:   models/res50.py
    	new file:   models/test128.png
    	new file:   models/test256.png
    	new file:   models/testres50.png
    	modified:   models/train.py
    	renamed:    models/models.py -> models/unet128.py
    	new file:   models/unet256.py
    
    * Cleaned the code.
    Made three different models.
    Generated scripts to train and do some simple testing.
    	new file:   models/test.py
    
    * Monodepth2 in process. SSIM Loss...
    	deleted:    ReadingMaterial/D3VO.pdf
    	deleted:    ReadingMaterial/Monodepth2_Paper.pdf
    	deleted:    ReadingMaterial/UNet.pdf
    	new file:   losses/ssim_loss.py
    	new file:   losses/ssim_tf.py
    	new file:   losses/ssim_usingcv2.py
    	new file:   models/monodepth2.py
    	modified:   models/test.py
    	modified:   models/test128.png
    	modified:   models/test256.png
    	modified:   models/train.py
    	modified:   models/unet128.py
    
    * deleted:    losses/__pycache__/ssim_loss.cpython-36.pyc
    	deleted:    losses/__pycache__/ssim_lossf.cpython-36.pyc

--- dataloaders/dataloaders.py
+++ dataloaders/dataloaders.py
@@ -1,13 +1,11 @@
-# rgb_path = '/tmp/Projects2021/rgbd_dataset/indoor_dataset/train_dataset/rgb/'
-
-
 import tensorflow as tf
 import numpy as np
 import math
 import cv2
 import os
+import keras
 
-class dataloarder_rgbd(tf.keras.utils.Sequence): #
+class dataloader_rgbd(tf.keras.utils.Sequence): #
     def __init__(self, dataset_path, batch_size, image_size=128, shuffle=False):
         self.rgb_images = os.listdir(str(str(dataset_path)+'/rgb/'))
         self.rgb_images.sort()
@@ -20,8 +18,19 @@ class dataloarder_rgbd(tf.keras.utils.Sequence): #
         # Shuffle later
         self.batch_size = batch_size
         self.shuffle = shuffle
-        self.on_epoch_end()
         self.image_size = image_size
+        self.min_depth = 999999
+        self.max_depth = 0
+        for depthimg in self.depth_images:
+            depth = cv2.imread(depthimg, -1)
+            depth = cv2.resize(depth, (self.image_size, self.image_size))
+            mx = np.max(depth)
+            mn = np.min(depth)
+            if mx > self.max_depth:
+                self.max_depth = mx
+            if mn < self.min_depth:
+                self.min_depth = mn
+        self.on_epoch_end()
 
     def on_epoch_end(self):
         None
@@ -31,7 +40,7 @@ class dataloarder_rgbd(tf.keras.utils.Sequence): #
 
     def __getitem__(self, index):
         rgb_batch = self.rgb_images[index * self.batch_size:(index + 1) * self.batch_size]
-        depth_batch = self.depth_batch[index * self.batch_size:(index + 1) * self.batch_size]
+        depth_batch = self.depth_images[index * self.batch_size:(index + 1) * self.batch_size]
         X, y = self.__get_data(rgb_batch, depth_batch)
         return X, y
 
@@ -39,22 +48,44 @@ class dataloarder_rgbd(tf.keras.utils.Sequence): #
         rgb_images=[]
         depth_images=[]
         for rgb_file in rgb_imgs:
-            rbg_img = cv2.imread(file_name) #, cv2.IMREAD_COLOR)
+            rbg_img = cv2.imread(rgb_file) #, cv2.IMREAD_COLOR)
             rgb_img = cv2.resize(rbg_img, (self.image_size, self.image_size))
-            rgb_image = rgb_image/np.max(rgb_image)
+            rgb_img = rgb_img/float(255.0)
             rgb_images.append(rgb_img)
             
         for depth_file in depth_imgs:
             depth_image = cv2.imread(depth_file, -1)
             depth_image = cv2.resize(depth_image, (self.image_size, self.image_size))
-            depth_image = depth_image/np.max(depth_image)
+            depth_image = depth_image/np.max(depth_image) #(depth_image-self.min_depth)/(self.max_depth - self.min_depth)
             depth_images.append(depth_image)
             
-        return np.array(rgb_images), np.array(depth_image)
-    
-    
-# dataset_path = '/tmp/Projects2021/rgbd_dataset/indoor_dataset/train_dataset'
+        return np.array(rgb_images, dtype=np.float16), np.array(depth_images, dtype=np.float16)
 
-# dtloader = dataloarder_rgbd(dataset_path,10)
-# print(dtloader.rgb_images[10])
-# print(dtloader.depth_images[10])
+    def get_testing_sample(self):
+        return self.__get_data(self.rgb_images[0:10], self.depth_images[0:10])
+    
+class DataGenerator(keras.utils.Sequence):
+  
+  def __init__(self, batch_size = 8, image_size = 128):
+    self.batch_size = batch_size
+    self.image_size = image_size
+
+  def load(self, rgb_file, depth_file):
+    image = cv2.imread(rgb_file)
+    image = cv2.resize(image, (self.image_size, self.image_size)) # resize...
+    depth = cv2.imread(depth_file, -1)
+    depth = cv2.resize(depth, (self.image_size, self.image_size)) 
+
+    image_normalized = image/np.max(image)
+    depth_normalized = depth/np.max(depth)
+    return image_normalized, depth_normalized
+
+  def load_all(self, rgb_files, depth_files):
+      images = []
+      depths = []
+      for i in range(len(rgb_files)):
+          img, dpth = self.load(rgb_files[i], depth_files[i])
+          images.append(img)
+          depths.append(dpth)
+
+      return np.array(images), np.array(depths)
--- models/train.py
+++ models/train.py
@@ -1,13 +1,54 @@
 import sys
 sys.path.insert(1, '/tmp/Projects2021/depth_estimation/final-project-monodepth-ccny/dataloaders/')
+sys.path.insert(1, '/tmp/Projects2021/depth_estimation/final-project-monodepth-ccny/losses/')
+from ssim_loss import *
 from dataloaders import *
-from models import *
+from unet128 import unet128
+from unet256_v2 import unet256_v2
+from res50 import res50
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
 
-dataset_path = '/tmp/Projects2021/rgbd_dataset/indoor_dataset/train_dataset'
-dtloader = dataloarder_rgbd(dataset_path,10)
+dataset_path = '/tmp/Projects2021/rgbd_dataset/dataset1/'
+argv = sys.argv
 
-m = models(128)
-m.unet_resnet50.compile(optimizer='adam', loss='mse')
-m.unet_resnet50.summary()
+if len(argv) > 1:
+    if argv[1] == 'unet128':
+        dtloader = dataloader_rgbd(dataset_path, 8, image_size=128)
+        checkpoint = ModelCheckpoint('best_model128.hdf5',
+                                    monitor='loss',
+                                    save_best_only=True)
 
-m.unet_resnet50.fit_generator(dtloader, samples_per_epoch=50, nb_epoch=3)
+        m = unet128(input_shape=[128, 128, 3])
+        m.model.compile(optimizer='adam', loss=ssim_loss())
+        m.model.summary()
+        m.model.fit(dtloader, epochs=int(argv[2]), callbacks=[checkpoint])
+
+    elif argv[1] == 'unet256':
+        dtloader = dataloader_rgbd(dataset_path, 8, image_size=256)
+        checkpoint = ModelCheckpoint('best_model256.hdf5',
+                                    monitor='loss',
+                                    save_best_only=True)
+
+        m = unet256_v2(input_shape=[256, 256, 3])
+        m.model.compile(optimizer='adam', loss='mse')
+        m.model.summary()
+        m.model.fit(dtloader, epochs=int(argv[2]), callbacks=[checkpoint])
+
+    elif argv[1] == 'res50':
+        dtloader = dataloader_rgbd(dataset_path, 8, image_size=128)
+        checkpoint = ModelCheckpoint('best_modelres50.hdf5',
+                                    monitor='loss',
+                                    save_best_only=True)
+
+        m = res50(input_shape=(128, 128, 3))
+        m.model.compile(optimizer='adam', loss='mse')
+        m.model.summary()
+        m.model.fit(dtloader, epochs=int(argv[2]), callbacks=[checkpoint])
+    else:
+        print("\nPlease define the model you want to train and number of epochs!")
+        print("Command Example: python3 train.py unet128 30\n")
+else:
+    print("\nPlease define the model you want to train and number of epochs!")
+    print("Command Example: python3 train.py unet128 30\n")
